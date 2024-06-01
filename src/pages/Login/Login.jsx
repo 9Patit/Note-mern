bb213979a6c3554f8371851e3cdbf744d16f37db
unknown <patit.sudsat@gmail.com>
2024-06-01 16:14:07 +0700

    add waiting page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,31 @@
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import PasswordInput from "../../components/Input/PasswordInput";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { validateEmail } from "../../utils/helper";
 import axiosInstance from "../../utils/axiosinstance";
 
 const Login = () => {
-  const [email, setEmail] = useState("");  
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const checkServerConnection = async () => {
+      try {
+        await axiosInstance.get("/"); // เรียก API เชื่อมต่อเซิร์ฟเวอร์หลัก
+        console.log("เชื่อมต่อกับเซิร์ฟเวอร์ได้");
+        setLoading(true);
+      } catch (error) {
+        console.error("ไม่สามารถเชื่อมต่อกับเซิร์ฟเวอร์ได้", error);
+      }
+    };
+
+    checkServerConnection();
+  }, []);
 
   const handleLogin = async (e) => {
     e.preventDefault();
@@ -28,12 +43,10 @@ const Login = () => {
 
     // Login API call
     try {
-      
       const response = await axiosInstance.post("/login", {
         email: email,
-        password: password
+        password: password,
       });
-
 
       // Handle successful login response
       if (response.data && response.data.accessToken) {
@@ -42,54 +55,70 @@ const Login = () => {
       }
     } catch (error) {
       // Handle login error
-      if (error.response && error.response.data && error.response.data.message ) {
-        setError(error.response.data.message)
+      if (
+        error.response &&
+        error.response.data &&
+        error.response.data.message
+      ) {
+        setError(error.response.data.message);
       } else {
-        setError("มีErrorที่ไม่คาดคิด กรุณาลองใหม่อีกครั้ง")
+        setError("มีErrorที่ไม่คาดคิด กรุณาลองใหม่อีกครั้ง");
       }
     }
   };
 
   return (
     <>
-      <Navbar />
+      {!loading ? (
+        <>
+          <Navbar />
+          <div className="flex items-center justify-center mt-28">
+            <div className="w-96 border rounded bg-white px-7 py-10">
+              <h1>Please wait a moment</h1>
+            </div>
+          </div>
+        </>
+      ) : (
+        <>
+          <Navbar />
+          <div className="flex items-center justify-center mt-28">
+            <div className="w-96 border rounded bg-white px-7 py-10">
+              <form onSubmit={handleLogin}>
+                <h4 className="text-2xl mb-7">เข้าสู่ระบบ</h4>
 
-      <div className="flex items-center justify-center mt-28">
-        <div className=" w-96 border rounded bg-white px-7 py-10">
-          <form onSubmit={handleLogin}>
-            <h4 className="text-2xl mb-7">เข้าสู่ระบบ</h4>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="input-box"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
 
-            <input
-              type="text"
-              placeholder="Email"
-              className="input-box"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
+                <PasswordInput
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
 
-            <PasswordInput
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+                {error && <p className="text-red-500 text-xs pb-1">{error}</p>}
 
-            {error && <p className="text-red-500 text-xs pb-1">{error} </p>}
+                <button type="submit" className="btn-primary">
+                  เข้าสู่ระบบ
+                </button>
 
-            <button type="submit" className="btn-primary">
-              เข้าสู่ระบบ
-            </button>
-
-            <p className="text-sm text-center mt-4">
-              ยังไม่ได้ลงทะเบียน?{" "}
-              <Link
-                to="/signup"
-                className=" font-medium text-primary underline"
-              >
-                สร้างบัญชี
-              </Link>
-            </p>
-          </form>
-        </div>
-      </div>
+                <p className="text-sm text-center mt-4">
+                  ยังไม่ได้ลงทะเบียน?{" "}
+                  <Link
+                    to="/signup"
+                    className="font-medium text-primary underline"
+                  >
+                    สร้างบัญชี
+                  </Link>
+                </p>
+              </form>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
